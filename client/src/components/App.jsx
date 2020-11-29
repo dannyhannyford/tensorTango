0e1542d35ece732b4f9f0e2941dfac5c39f6a6ec
dannyhannyford <dannyhannyford@gmail.com>
2020-11-29 15:38:43 -0800

    posenet detects and draws keypoints on webcam

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,39 +2,81 @@ import React, { useRef } from 'react';
 import * as tf from '@tensorflow/tfjs';
 import * as poseNet from '@tensorflow-models/posenet';
 import Webcam from 'react-webcam';
+import { drawKeypoints, drawSkeleton } from '../utilities';
 
 const App = () => {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
+
+  const runPosenet = async () => {
+    const net = await poseNet.load({
+      inputResolution: { width: 640, height: 480 },
+      // scale: smaller is faster/less accurate
+      scale: 0.1,
+    });
+
+    setInterval(() => {
+      detect(net);
+    }, 10000);
+  };
+
+  const detect = async (net) => {
+    if (typeof webcamRef.current !== 'undefined' && webcamRef.current !== null && webcamRef.current.video.readyState === 4) {
+      // video properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+      // setting video dimensions
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+      // detections
+      const pose = await net.estimateSinglePose(video);
+      console.log('detect pose: ', pose);
+
+      drawCanvas(pose, video, videoWidth, videoHeight, canvasRef);
+    }
+  };
+
+  const drawCanvas = (pose, video, videoWidth, videoHeight, canvas) => {
+    const ctx = canvas.current.getContext('2d');
+    canvas.current.width = videoWidth;
+    canvas.current.height = videoHeight;
+
+    drawKeypoints(pose['keypoints'], 0.5, ctx);
+    drawSkeleton(pose['keypoints'], 0.5, ctx);
+  };
+
+  runPosenet();
+
   return (
     <div>
-      <Webcam 
-      ref={webcamRef}
-      style={{
-        position: 'absolute',
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        left: 0,
-        right: 0,
-        textAlign: 'center',
-        zindex: 9,
-        width: 640,
-        height: 480,
-      }}
+      <Webcam
+        ref={webcamRef}
+        style={{
+          position: 'absolute',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          left: 0,
+          right: 0,
+          textAlign: 'center',
+          zindex: 9,
+          width: 640,
+          height: 480,
+        }}
       />
-      <canvas 
-      ref={canvasRef}
-      style ={{
-        position: 'absolute',
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        left: 0,
-        right: 0,
-        textAlign: 'center',
-        zindex: 9,
-        width: 640,
-        height: 480,
-      }}
+      <canvas
+        ref={canvasRef}
+        style={{
+          position: 'absolute',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          left: 0,
+          right: 0,
+          textAlign: 'center',
+          zindex: 9,
+          width: 640,
+          height: 480,
+        }}
       />
     </div>
   );
