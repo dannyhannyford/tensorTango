a35decc9a7033a5f16ae96de83b6d7395418823b
dannyhannyford <dannyhannyford@gmail.com>
2020-11-29 14:41:37 -0800

    webcam and canvas components work

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import * as tf from '@tensorflow/tfjs';
+import * as poseNet from '@tensorflow-models/posenet';
+import Webcam from 'react-webcam';
 
 const App = () => {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
   return (
-    <div>hi</div>
-  )
+    <div>
+      <Webcam 
+      ref={webcamRef}
+      style={{
+        position: 'absolute',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        left: 0,
+        right: 0,
+        textAlign: 'center',
+        zindex: 9,
+        width: 640,
+        height: 480,
+      }}
+      />
+      <canvas 
+      ref={canvasRef}
+      style ={{
+        position: 'absolute',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        left: 0,
+        right: 0,
+        textAlign: 'center',
+        zindex: 9,
+        width: 640,
+        height: 480,
+      }}
+      />
+    </div>
+  );
 };
 
 export default App;
