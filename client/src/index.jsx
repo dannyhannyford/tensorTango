c02e1a3240718b2352f117883c4b414ac9761e94
dannyhannyford <dannyhannyford@gmail.com>
2020-12-19 14:52:09 -0800

    app wrapped in auth0provider

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,14 +4,15 @@ import { Auth0Provider } from '@auth0/auth0-react';
 import App from './components/App';
 import 'regenerator-runtime/runtime';
 import './style.css';
- 
+import { ClientId, domain } from '../../config';
+
 ReactDOM.render(
   <Auth0Provider
-    domain="YOUR_AUTH0_DOMAIN"
-    clientId="YOUR_AUTH0_CLIENT_ID"
+    domain={domain}
+    clientId={ClientId}
     redirectUri={window.location.origin}
   >
     <App />
   </Auth0Provider>,
-  document.getElementById('app')
+  document.getElementById('app'),
 );
